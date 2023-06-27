eccdf4966b0de0b6e174ac1b45e97b2040970359
luka1104 <skyward.luke11@gmail.com>
2023-06-28 00:19:54 +0900

    update api

--- api/handleReserve/index.ts
+++ api/handleReserve/index.ts
@@ -1,75 +1,23 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import Web3 from "web3"
-import contract from "./abis/NhouseNFT.json"
+import abi from "./abis/NhouseNFT.json"
 const API_URL = process.env.API_URL || "https://evm.astar.network"
 const PUBLIC_KEY = process.env.PUBLIC_KEY || "0x872449c44937f6Ac266cbBCDCb189B25AcEBb9E9"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || "0xBE0505c227A3f786319f820510F9C09BB79EAb74"
-
-const handleTransfer = async (web3: any, address: string, tokenId: string) => {
-  const nftContract = new web3.eth.Contract((contract as any).abi, CONTRACT_ADDRESS)
-  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest")
-  const data = await nftContract.methods.transferFrom(PUBLIC_KEY, address, JSON.parse(tokenId)).encodeABI()
-  const tx = {
-    gas: 500000,
-    to: CONTRACT_ADDRESS,
-    nonce: nonce,
-    data: data,
-    from: address,
-  }
-  const resp = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY, async (err: any, signedTx: any) => {
-    if (err) {
-      console.log("ERROR", err)
-      return
-    }
-    console.log("SIGNING", signedTx)
-    await web3.eth.sendSignedTransaction(signedTx.rawTransaction as string, (err: any, resp: any) => {
-      if (err) {
-        // context.res = {
-        //   status: 500,
-        //   message: err,
-        // }
-        console.log("ERROR", err)
-        return
-      }
-      console.log("RESERVING", resp)
-    })
-  })
-  return resp.transactionHash
-}
+import { ethers } from "ethers"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const { address, tokenId } = req.body
-  const web3 = new Web3(API_URL)
-  const resp = await handleTransfer(web3, address, tokenId)
-  console.log(resp)
-
-  if (resp) {
-    const interval = setInterval(function () {
-      console.log("Attempting to get transaction receipt...")
-      web3.eth.getTransactionReceipt(resp, function (err, rec) {
-        if (rec) {
-          console.log(rec, "Transaction receipt received")
-          clearInterval(interval)
-          context.res = {
-            status: 200,
-            body: rec,
-          }
-        }
-        if (err) {
-          console.log(err, "Error getting transaction receipt")
-          clearInterval(interval)
-          context.res = {
-            status: 500,
-          }
-        }
-      })
-    }, 1000)
-  } else {
-    console.log("Error getting transaction hash")
-    context.res = {
-      status: 500,
-    }
+  const provider = new ethers.providers.JsonRpcProvider(API_URL)
+  const signer = new ethers.Wallet(PRIVATE_KEY, provider)
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, (abi as any).abi, signer)
+  const nonce = await provider.getTransactionCount(PUBLIC_KEY)
+  const tx = await contract.transferFrom(PUBLIC_KEY, address, JSON.parse(tokenId), { nonce: nonce })
+  console.log(tx)
+  await tx.wait()
+  context.res = {
+    status: 200,
+    body: tx,
   }
 }
 
