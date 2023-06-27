c4951a4ba06a8b566ac9a9bf4b08578b77837838
luka1104 <skyward.luke11@gmail.com>
2023-06-28 00:23:06 +0900

    Refactor mintOwnership contract interaction

--- api/mintOwnership/index.ts
+++ api/mintOwnership/index.ts
@@ -1,47 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import Web3 from "web3"
-import contract from "./abis/NhouseMembership.json"
-
+import abi from "./abis/NhouseMembership.json"
 const API_URL = process.env.API_URL || "https://evm.astar.network"
 const PUBLIC_KEY = process.env.PUBLIC_KEY || "0x872449c44937f6Ac266cbBCDCb189B25AcEBb9E9"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const CONTRACT_ADDRESS = process.env.MEMBERSHIP_ADDRESS || "0x7B4a600b78fC6534B4125145cd38e45d366ebD28"
-
-const handleMint = async (web3: any, address: string, tokenUri: any, context: Context) => {
-  const nftContract = new web3.eth.Contract((contract as any).abi, CONTRACT_ADDRESS)
-  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest")
-  const data = await nftContract.methods.mintNFT(address, tokenUri).encodeABI()
-  const tx = {
-    gas: 5000000,
-    to: CONTRACT_ADDRESS,
-    nonce: nonce,
-    data: data,
-    from: PUBLIC_KEY,
-  }
-  const resp = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY, async (err: any, signedTx: any) => {
-    if (err) {
-      context.res = {
-        status: 500,
-      }
-      return
-    }
-    context.log("SIGNING", signedTx)
-    await web3.eth.sendSignedTransaction(signedTx.rawTransaction as string, (err: any, resp: any) => {
-      if (err) {
-        context.res = {
-          status: 500,
-        }
-        return
-      }
-      context.log("RESERVING", resp)
-    })
-  })
-  return { status: 200, hash: resp.transactionHash }
-}
+import { ethers } from "ethers"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const { address } = req.body
-  const web3 = new Web3(API_URL)
 
   const tokenUri = JSON.stringify({
     name: "Nhouse Membership NFT",
@@ -49,30 +15,24 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     image: "https://art.pixilart.com/82d984fcd46cafb.gif",
     propertyName: "Nhouse Blue",
   })
-
-  const resp = await handleMint(web3, address, tokenUri, context)
-
-  if (resp.hash) {
-    const interval = setInterval(function () {
-      context.log("Attempting to get transaction receipt...")
-      web3.eth.getTransactionReceipt(resp.hash, function (err, rec) {
-        if (rec) {
-          context.log(rec)
-          clearInterval(interval)
-          context.res = {
-            status: 200,
-            body: rec,
-          }
-        }
-        if (err) {
-          context.log(err)
-          clearInterval(interval)
-          context.res = {
-            status: 500,
-          }
-        }
-      })
-    }, 1000)
+  const provider = new ethers.providers.JsonRpcProvider(API_URL)
+  const signer = new ethers.Wallet(PRIVATE_KEY, provider)
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, (abi as any).abi, signer)
+  const nonce = await provider.getTransactionCount(PUBLIC_KEY)
+  try {
+    const tx = await contract.mintNFT(address, tokenUri, { nonce: nonce })
+    context.log(tx)
+    await tx.wait()
+    context.res = {
+      status: 200,
+      body: tx,
+    }
+  } catch (error) {
+    context.log(error)
+    context.res = {
+      status: 500,
+      body: error,
+    }
   }
 }
 
