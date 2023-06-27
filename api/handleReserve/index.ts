e2bea57ae8612b198de4bc1e97dbf413ac61d6c2
luka1104 <skyward.luke11@gmail.com>
2023-06-28 00:21:09 +0900

    Handle Reserve transfers with error handling

--- api/handleReserve/index.ts
+++ api/handleReserve/index.ts
@@ -12,12 +12,20 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
   const signer = new ethers.Wallet(PRIVATE_KEY, provider)
   const contract = new ethers.Contract(CONTRACT_ADDRESS, (abi as any).abi, signer)
   const nonce = await provider.getTransactionCount(PUBLIC_KEY)
-  const tx = await contract.transferFrom(PUBLIC_KEY, address, JSON.parse(tokenId), { nonce: nonce })
-  console.log(tx)
-  await tx.wait()
-  context.res = {
-    status: 200,
-    body: tx,
+  try {
+    const tx = await contract.transferFrom(PUBLIC_KEY, address, JSON.parse(tokenId), { nonce: nonce })
+    context.log(tx)
+    await tx.wait()
+    context.res = {
+      status: 200,
+      body: tx,
+    }
+  } catch (error) {
+    context.log(error)
+    context.res = {
+      status: 500,
+      body: error,
+    }
   }
 }
 
