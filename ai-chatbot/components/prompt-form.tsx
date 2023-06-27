84a74c8c13f4b331b3ebe78ec8b25dfb7685acda
luka1104 <skyward.luke11@gmail.com>
2023-06-28 01:18:19 +0900

    Add muted text color to prompt form input field

--- ai-chatbot/components/prompt-form.tsx
+++ ai-chatbot/components/prompt-form.tsx
@@ -60,7 +60,7 @@ export function PromptForm({
         onChange={e => setInput(e.target.value)}
         placeholder="AI コンシェルジュに話しかけてみましょう。"
         spellCheck={false}
-        className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+        className="text-muted-foreground min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
       />
       <div className="absolute right-0 top-4 sm:right-4">
         <Tooltip>
