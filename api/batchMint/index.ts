dd7686c8209fcf336851b85428f807f17559afc7
luka1104 <skyward.luke11@gmail.com>
2023-06-28 00:27:00 +0900

    Refactor batch minting with ethers

--- api/batchMint/index.ts
+++ api/batchMint/index.ts
@@ -1,46 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import Web3 from "web3"
-import contract from "./abis/NhouseNFT.json"
+import { ethers } from "ethers"
+import abi from "./abis/NhouseNFT.json"
 const API_URL = process.env.API_URL || "https://evm.astar.network"
 const PUBLIC_KEY = process.env.PUBLIC_KEY || "0x872449c44937f6Ac266cbBCDCb189B25AcEBb9E9"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || "0xBE0505c227A3f786319f820510F9C09BB79EAb74"
 
-const handleBatchMint = async (web3: any, tokenUris: any[], context: Context) => {
-  const nftContract = new web3.eth.Contract((contract as any).abi, CONTRACT_ADDRESS)
-  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest")
-  const data = await nftContract.methods.batchMintNFT(PUBLIC_KEY, tokenUris).encodeABI()
-  const tx = {
-    gas: 5000000,
-    to: CONTRACT_ADDRESS,
-    nonce: nonce,
-    data: data,
-    from: PUBLIC_KEY,
-  }
-  const resp = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY, async (err: any, signedTx: any) => {
-    if (err) {
-      context.res = {
-        status: 500,
-      }
-      return
-    }
-    context.log("SIGNING", signedTx)
-    await web3.eth.sendSignedTransaction(signedTx.rawTransaction as string, (err: any, resp: any) => {
-      if (err) {
-        context.res = {
-          status: 500,
-        }
-        return
-      }
-      context.log("RESERVING", resp)
-    })
-  })
-  return { status: 200, hash: resp.transactionHash }
-}
-
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const { dates } = req.body
-  const web3 = new Web3(API_URL)
 
   let tokenUris: any[] = []
   dates.map((date: any) => {
@@ -55,29 +22,26 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     )
   })
 
-  const resp = await handleBatchMint(web3, tokenUris, context)
+  const { address } = req.body
 
-  if (resp.hash) {
-    const interval = setInterval(function () {
-      context.log("Attempting to get transaction receipt...")
-      web3.eth.getTransactionReceipt(resp.hash, function (err, rec) {
-        if (rec) {
-          context.log(rec)
-          clearInterval(interval)
-          context.res = {
-            status: 200,
-            body: rec,
-          }
-        }
-        if (err) {
-          context.log(err)
-          clearInterval(interval)
-          context.res = {
-            status: 500,
-          }
-        }
-      })
-    }, 1000)
+  const provider = new ethers.providers.JsonRpcProvider(API_URL)
+  const signer = new ethers.Wallet(PRIVATE_KEY, provider)
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, (abi as any).abi, signer)
+  const nonce = await provider.getTransactionCount(PUBLIC_KEY)
+  try {
+    const tx = await contract.batchMintNFT(address, tokenUris, { nonce: nonce })
+    context.log(tx)
+    await tx.wait()
+    context.res = {
+      status: 200,
+      body: tx,
+    }
+  } catch (error) {
+    context.log(error)
+    context.res = {
+      status: 500,
+      body: error,
+    }
   }
 }
 
